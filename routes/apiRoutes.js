55bffe49fef82112f54dbec9bc71b757e154a526
schaparro08 <101684009+schaparro08@users.noreply.github.com>
2022-04-29 16:30:00 -0400

    addition of html routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,10 @@
 // TODO: Require the router and db items needed
-
+const router = require('express').Router();
+const store = require('../db/store')
 // TODO: Set up a get/post/delete methods as responses to the database
 
+
+
 // TODO: Export the router
+
+module.exports = router;
